d1a54b53786d2151142d53ddf9bed6840cd4d902
Ayush Kumar <ayushkumar54285@gmail.com>
2025-11-16 23:55:48 +0530

    1st change by TL

--- NTTools905/JunitProj01/src/main/java/com/nt/service/A1.java
+++ NTTools905/JunitProj01/src/main/java/com/nt/service/A1.java
@@ -3,6 +3,6 @@ package com.nt.service;
 
 public class A1 {
 	public String sayHello() {
-		return "Good Morning! this is git tutorial part";
+		return "Good Morning! this is git tutorial part + 1st change by TL";
 	}
 }
